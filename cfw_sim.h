7987b97dd7a165dc84783297f58679c0dada4098
shengpan <shengpan@rdamicro.com>
2017-05-27 17:31:26 +0800

    2ndbranch changed

--- cfw_sim.h
+++ cfw_sim.h
@@ -14,6 +14,9 @@ UINT32 CFW_SimTest(UINT8 Index);
 
 
 VOID cfw_PLMNtoBCD(UINT8 *pIn, UINT8 *pOut, UINT8 *nInLength);
+
+
+
 VOID cfw_BCDtoPLMN(UINT8 *pIn, UINT8 *pOut, UINT8 nInLength);
 VOID cfw_IMSItoASC(UINT8 *InPut, UINT8 *OutPut, UINT8 *OutLen);
 
